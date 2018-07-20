7b0da8461bd5aa71ca1f83431d154e3a3fa1ae16
HunterZhao <zhaohevip@gmail.com>
2018-07-21 03:54:34 +0800

    Initial commit

---
+++ devtool.rb
@@ -0,0 +1,36 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Devtool < Formula
+  desc "An all-round developer toolset."
+  version "1.0"
+  homepage "https://github.com/MrHunterZhao/DevTool"
+  url "https://github.com/MrHunterZhao/DevTool/releases/download/v#{version}/devtool"
+  sha256 "3fafdb2e2dfd5a01a0cdf39f8d704134a75d7459ec0d6b390954d02a21842e62"
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    #system "./configure", "--disable-debug",
+    #                      "--disable-dependency-tracking",
+    #                      "--disable-silent-rules",
+    #                      "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    #system "make", "install" # if this fails, try separate make/make install steps
+    bin.install "devtool"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test DevTool`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
